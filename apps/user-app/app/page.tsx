badf7626aaeb1b10266dc5424829eab353bdad2b
Hemant <davidbroddy1@gmail.com>
2024-10-14 17:57:34 +0530

    Introduced authentication using NextAuth

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import {useBalance} from "@repo/store/useBalance"
+import {AppBar} from "@repo/ui/appbar"
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const balance = useBalance();
-
+  const session = useSession();
   return (
-    <div className="text-lg bg-green-400">
-      hi there{balance}
+    <div className="text-lg">
+      <AppBar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
     </div>
   );
 }
